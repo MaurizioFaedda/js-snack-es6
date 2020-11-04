2af5ee35e9f6494a6682d51bfb719a1e71525959
Maurizio <maurizio.faedda1@gmail.com>
2020-11-04 17:14:30 +0100

    Creata nuova copia con map

---
+++ main3.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    const nomi = ['mario', 'luca', 'luigi', 'maria', 'sara', 'mauro', 'alice'];
+
+
+
+    let nomi_copia = nomi.map((element) =>
+    element);
+
+
+    console.log(nomi_copia);
+})
