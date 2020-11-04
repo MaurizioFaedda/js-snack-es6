726f5d4bba3a7055f9303ced23dc8df8a92604d8
Maurizio <maurizio.faedda1@gmail.com>
2020-11-04 17:33:23 +0100

    generata lettera random

--- main3.js
+++ main3.js
@@ -7,6 +7,28 @@ $(document).ready(function() {
     let nomi_copia = nomi.map((element) =>
     element);
 
+    nomi_copia.forEach((element) => {
+
+        element.position = {
+            position : getRandomChar()
+        };
+    });
+
+
 
     console.log(nomi_copia);
+
 })
+
+function getRandomChar() {
+    const abc = 'abcdefghijklmnopqrstuvwxyz';
+
+    const posizione = getRndInteger(0, 25);
+
+    const lettera_random = abc.charAt(posizione);
+    return lettera_random;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
