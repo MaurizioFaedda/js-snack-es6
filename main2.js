c6ad4919338bb13473ff2b200147ac36d1e3788e
Maurizio <maurizio.faedda1@gmail.com>
2020-11-04 16:38:12 +0100

    condizione per pushare l'elemento nella 'i' posizione con indexof

--- main2.js
+++ main2.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
     console.log(inizio, fine);
 
-    let nomi_scelti = nomi.filter((element) => element > inizio || element < fine ); // arrow function con return implicito
+    let nomi_scelti = nomi.filter((element) => (indexOf[element] > inizio || indexOf[element] < fine)); // arrow function con return implicito
     console.log(nomi_scelti);
 
     // var nomi_estratti = [];
