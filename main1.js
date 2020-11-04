819b2953f7062f7990d7dda8f479f2cbac5c1eb0
Maurizio <maurizio.faedda1@gmail.com>
2020-11-04 14:49:43 +0100

    inizio struttura snack1 con es6

---
+++ main1.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // creo un array di oggetti
+    var macchine = [
+        {
+            nome : 'fiat',
+            peso : 50
+        },
+        {
+            nome : 'ford',
+            peso : 70
+        },
+        {
+            nome : 'jeep',
+            peso : 90
+        }
+    ];
+
+    // voglio trovare il peso minimo usando la nuova sintassi es6
+    
+})
